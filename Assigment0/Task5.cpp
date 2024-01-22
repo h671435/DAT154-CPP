5dd5b3db46373149244407e0aef40ae86ee7208f
Storm <sangolts@gmail.com>
2024-01-22 21:00:49 +0100

    Litt av task5

--- Assigment0/Task5.cpp
+++ Assigment0/Task5.cpp
@@ -1,7 +1,100 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class Car {
+public:
+    int number;
+    int buyPrice;
+    int sellPrice;
+
+    Car(int nr, int bprice, int sprice) : number(nr), buyPrice(bprice), sellPrice(sprice) {
+    }
+};
+
+class CarManager {
+private:
+    vector<Car> cars;
+
+public:
+    void BuyCar() {
+        int nr, buy;
+        wcout << L"Enter nr:";
+        wcin >> nr;
+        wcout << L"Enter buy price:";
+        wcin >> buy;
+        cars.push_back(Car(nr, buy, 0));
+    }
+
+    void SellCar() {
+        int nr, sell;
+        wcout << L"Enter nr:";
+        wcin >> nr;
+        wcout << L"Enter sell price:";
+        wcin >> sell;
+
+        bool carFound = false;
+
+        for (auto &car: cars) {
+            if (car.number == nr) {
+                car.sellPrice = sell;
+                carFound = true;
+                wcout << L"Updated sell price for car " << nr << L" to: " << sell << "\n";
+                break;
+            }
+        }
+
+        if (!carFound) {
+            wcout << L"Couldn't find car with number " << nr << "\n";
+        }
+    }
+
+    void ListCars() {
+        for (int i = 0; i < n; i++)
+            wcout << L"Carnr: " << *(pnr + i) << L" Price: " << *(pbuy + i) << endl;
+    }
+
+    void FindProfit() {
+        int profit = 0;
+
+        for (int i = 0; i < n; i++) {
+            profit -= bprice[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            profit += sprice[i];
+        }
+
+        wcout << L"The total profit of the cars are: " << profit << "\n";
+    }
+};
+
+
+
+
 int main() {
-    wcout << L"Heisann alle sammen! :) TEST"
-    return 0;
+    CarManager cm;
+    int choice = 0;
+    do {
+        wcout << endl << L"List of choices" << endl;
+        wcout << L"1 Buy a car" << endl;
+        wcout << L"2 Sell a car" << endl;
+        wcout << L"3 Find the profit" << endl;
+        wcout << L"4 List all the cars" << endl;
+        wcout << L"5 Cleanup list (remove sold cars)" << endl;
+        wcout << L"0 Quit" << endl;
+        wcin >> choice;
+
+        switch (choice) {
+                int nr, buy, i, sell;
+            case 1: cm.BuyCar();
+                break;
+            case 2: cm.SellCar();
+                break;
+            case 3: cm.FindProfit();
+                break;
+            case 4: cm.ListCars();
+                break;
+        }
+    } while (choice != 0);
 }
